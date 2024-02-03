9467b3c9bc4944f6708ff68225c7185e0117e266
landresPC <lsousa.andre@gmail.com>
2024-02-03 18:55:58 +0000

    add books class

---
+++ Book.py
@@ -0,0 +1,71 @@
+
+
+class Author:
+    def __init__(self, authorId="0000", name="john doe", alive=True, booksWritten=[], nationality="PT"):
+        self.authorId = authorId
+        self.name = name
+        self.alive = alive
+        self.booksWritten = booksWritten
+        self.nationality = nationality
+
+    def addWrittenBook(self, book):
+        assert type(book) == Book
+        if not book in self.booksWritten:
+            self.booksWritten.append(book)
+            return True
+        return False
+
+    def getName(self):
+        return self.name
+
+
+    def __eq__(self, other):
+        if type(other) != type(self):
+            return False
+        return self.authorId == self.authorId
+
+    def __str__(self):
+        return f'--[{self.authorId}]name: {self.name}; nationality: {self.nationality}; number of books: {str(len(self.booksWritten))}'
+
+        
+    def __repr__(self):
+        return str(self)
+
+
+class Book:
+    def __init__(self, bookId="0000", title="testTitle", author=None, nPages=1, volume="vol0"):
+        self.bookId = bookId
+        self.title = title
+        self.author = author
+        self.nPages = nPages
+        self.volume = volume
+
+
+    def updateAuthor(self, author):
+        assert type(author) == Author
+        self.author = author
+
+
+    def __eq__(self, other):
+        if type(other) != type(self):
+            return False
+
+        return self.bookId == other.bookId
+
+    def __str__(self):
+        return f'--[{self.bookId}]Title: {self.title}; author: {self.author.getName()}; pages: {self.nPages}--'
+
+
+    def __repr__(self):
+        return str(self)
+
+
+
+if __name__ == "__main__":
+    a0 = Author()
+    a1 = Author("0010", "Ana", False, [], "PT")
+    b0 = Book()
+    b0.updateAuthor(a0)
+    a0.addWrittenBook(b0)
+    b1 = Book("0001", "mah name eh jeff", a1, 22,"Vol 1")
+    a1.addWrittenBook(b1)
---
+++ Books.py
@@ -0,0 +1,78 @@
+from Book import Book, Author
+import pickle
+
+def saveState(state, tag="W_"):
+    with open(f'{tag}state.pkl', 'wb') as fp:
+        pickle.dump(state, fp)
+
+def getState(tag="W_"):
+    with open(f'{tag}state.pkl', 'rb') as fp:
+        return pickle.load(fp)
+
+def getBooksSavedState():
+    newBooks = Books()
+    newBooks.authors = getState("authors_")
+    newBooks.books = getState("books_")
+    return newBooks
+
+
+class Books:
+    def __init__(self):
+        self.books =[]
+        self.authors = []
+
+    def _saveState(self):
+        saveState(self.authors, "authors_")
+        saveState(self.books, "books_")
+
+
+        
+
+
+    def addAuthor(self, name, nationality, alive):
+        newKey = str(len(self.authors)+1)
+        if name in [a.name for a in self.authors]:
+            print("Author already exists")
+            return False
+        
+        self.authors.append(Author(newKey, name, alive, [], nationality))
+        return True
+
+    def addBook(self, title, nPages, volume, authorName):
+        newKey = str(len(self.books)+1)
+        if title in [b.title for b in self.books]:
+            print("Book Title already exists")
+            return False
+
+        author = self.getAuthorByName(authorName)
+        newBook = Book(newKey, title, author, nPages, volume)
+        self.books.append(newBook)
+        author.addWrittenBook(newBook)
+        
+        self._saveState()
+        
+        return True
+
+    def getBooksByIds(self, bookIds=[]):
+        currentBooks = {b.bookId:b for b in self.books}
+     
+        
+    def getAuthorByName(self, name):
+        for a in self.authors:
+            if a.name == name:
+                return a
+        return None
+
+    def getAuthorById(self, authorId):
+        for a in self.authors:
+            if a.authorId == authorId:
+                return a
+        return None
+
+
+
+
+
+if __name__ == "__main__":
+    bs1 = getBooksSavedState()
+
