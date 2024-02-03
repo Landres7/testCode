e287cec38e31d3719eaa6608316faa9e78e8c7ea
landresPC <lsousa.andre@gmail.com>
2024-02-03 18:50:19 +0000

    add books class

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
-
+@app.get("/hey")
+def read_root():
+    return "hello! How are you?"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
